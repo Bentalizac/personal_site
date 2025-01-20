eaf517202e5a9420aac303130afe5f16bc77586c
Eli Ellsworth <bentalizac@gmail.com>
2025-01-20 15:12:00 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Simple Webpage</h1>
+      <p>This is a bare-bones React app using TSX.</p>
+    </div>
+  );
+};
+
+export default App;
